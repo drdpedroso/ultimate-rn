e13b7379d1538917c938cf26fd06649aff00b7af
Eduardo <drdpedroso@gmail.com>
2019-12-04 17:56:14 -0300

    Add router

--- App.tsx
+++ App.tsx
@@ -1,19 +1,23 @@
 import React from 'react'
-import { StyleSheet, Text, View } from 'react-native'
+import { View, Text } from 'react-native'
+import { createAppContainer } from 'react-navigation'
+import { createStackNavigator } from 'react-navigation-stack'
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-    </View>
-  )
+class HomeScreen extends React.Component {
+  render() {
+    return (
+      <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+        <Text>Home Screen</Text>
+      </View>
+    )
+  }
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+const AppNavigator = createStackNavigator({
+  Home: {
+    screen: HomeScreen,
   },
 })
+
+// @ts-ignore
+export default createAppContainer(AppNavigator)
